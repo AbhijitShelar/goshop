f1b31e3ebedb0b5b7b0f2383097a088d9f5d6541
Abhi22012002 <abhijeetshelar70@gmail.com>
2023-10-08 20:32:56 +0530

    Responsive Navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
-import {FiShoppingCart} from "react-icons/fi";
-
+import { FiShoppingCart } from "react-icons/fi";
+import { CgMenu, CgClose } from "react-icons/cg";
 
 const Navbar = () => {
+  const [menuIcon, setMenuIcon] = useState();
+
   const Nav = styled.nav`
     .navbar-lists {
       display: flex;
@@ -136,7 +138,7 @@ const Navbar = () => {
           font-size: 4.2rem;
         }
       }
-      .cart-trolley--link {
+      .cart-trolley-link {
         position: relative;
 
         .cart-trolley {
@@ -144,7 +146,7 @@ const Navbar = () => {
           font-size: 5.2rem;
         }
 
-        .cart-total--item {
+        .cart-total-item {
           width: 4.2rem;
           height: 4.2rem;
           font-size: 2rem;
@@ -161,35 +163,68 @@ const Navbar = () => {
 
   return (
     <Nav>
-      <div className="navbar">
+      <div className={menuIcon ? "navbar active" : "navbar"}>
         <ul className="navbar-lists">
           <li>
-            <NavLink to="/" className="navbar-link ">
+            <NavLink
+              to="/"
+              className="navbar-link "
+              onClick={() => setMenuIcon(false)}
+            >
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/about" className="navbar-link ">
+            <NavLink
+              to="/about"
+              className="navbar-link "
+              onClick={() => setMenuIcon(false)}
+            >
               About
             </NavLink>
           </li>
           <li>
-            <NavLink to="/products" className="navbar-link ">
+            <NavLink
+              to="/products"
+              className="navbar-link "
+              onClick={() => setMenuIcon(false)}
+            >
               Products
             </NavLink>
           </li>
           <li>
-            <NavLink to="/contact" className="navbar-link ">
+            <NavLink
+              to="/contact"
+              className="navbar-link "
+              onClick={() => setMenuIcon(false)}
+            >
               Contact
             </NavLink>
           </li>
           <li>
-            <NavLink to='/cart' className="navbar-link cart-trolley-link">
-                <FiShoppingCart className='cart-trolley' />
-                <span className="cart-total-item">0</span>
+            <NavLink
+              to="/cart"
+              className="navbar-link cart-trolley-link"
+              onClick={() => setMenuIcon(false)}
+            >
+              <FiShoppingCart className="cart-trolley" />
+              <span className="cart-total-item">0</span>
             </NavLink>
           </li>
         </ul>
+
+        <div className="mobile-navbar-btn">
+          <CgMenu
+            name="menu-outline"
+            className="mobile-nav-icon"
+            onClick={() => setMenuIcon(true)}
+          />
+          <CgClose
+            name="close-outline"
+            className="mobile-nav-icon close-outline"
+            onClick={() => setMenuIcon(false)}
+          />
+        </div>
       </div>
     </Nav>
   );
